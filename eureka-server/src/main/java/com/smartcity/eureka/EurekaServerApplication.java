29cefae57a25c4335ccd8d5db23334819e6bf459
Thiago Amaral Maduro <thiagoamaralmaduro@Thiagos-MacBook-Pro.local>
2025-05-10 11:15:33 +0100

    Eureka server configurations

--- eureka-server/src/main/java/com/smartcity/eureka/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/smartcity/eureka/EurekaServerApplication.java
@@ -2,8 +2,11 @@ package com.smartcity.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+// This class starts the Eureka Server
 @SpringBootApplication
+@EnableEurekaServer // Enables Eureka Server functionality
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
