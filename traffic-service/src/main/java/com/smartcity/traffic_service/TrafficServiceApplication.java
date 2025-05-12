9e489a5a0cd2ed13a4d1ad2a21a9ce62ac2fc7fe
Thiago Amaral Maduro <thiagoamaralmaduro@Thiagos-MacBook-Pro.local>
2025-05-12 18:01:19 +0100

    Create the traffic-serve set up

---
+++ traffic-service/src/main/java/com/smartcity/traffic_service/TrafficServiceApplication.java
@@ -0,0 +1,13 @@
+package com.smartcity.traffic_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrafficServiceApplication.class, args);
+	}
+
+}
---
+++ traffic-service/src/test/java/com/smartcity/traffic_service/TrafficServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartcity.traffic_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafficServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
